f038b3987b6eaa10e46aaaf11c33609eb5501722
Jason Harwig <jason.harwig@gristmillexchange.com>
2024-08-08 12:03:51 -0500

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ssyc-cam/",
   build: {
     outDir: "docs",
     assetsDir: "./",
